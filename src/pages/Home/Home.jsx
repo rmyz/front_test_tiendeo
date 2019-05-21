90ba6fbacdc41f4605e86d53569d21101bcb5c10
Sergi Romeu <sergi.romeu@travelport.com>
2019-05-21 16:13:59 +0200

    refactor: improve function

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -42,7 +42,7 @@ export const Home = () => {
 
   const handleChange = event => {
     setOrderBy(event.target.value);
-    setItems(orderItems(event.target.value));
+    setItems(orderItems(event.target.value, items));
   };
 
   const addItem = item => {
