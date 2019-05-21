948a6131b38376d472f34c763b652de35668df42
Sergi Romeu <sergi.romeu@travelport.com>
2019-05-21 16:14:23 +0200

    test: add testing for home

--- src/pages/Home/__tests__/Home.spec.jsx
+++ src/pages/Home/__tests__/Home.spec.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import Home from "..";
-
 import { mount } from "enzyme";
+import Home from "..";
+import { StoreProvider } from "../../../store";
+import { initialItems } from "../../../utils";
 
 describe("[Home] page", () => {
-  const wrapper = mount(<Home />);
+  const wrapper = mount(
+    <StoreProvider>
+      <Home />
+    </StoreProvider>
+  );
+
   it("renders without crashing", () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it("should render the expected layout", () => {
+    expect(wrapper.find("Select")).toHaveLength(1);
+    expect(wrapper.find("CardWrapper")).toHaveLength(1);
+    expect(wrapper.find("Dialog")).toHaveLength(1);
+    expect(wrapper.find("Fab")).toHaveLength(1);
+    expect(wrapper.find("AddIcon")).toHaveLength(1);
+  });
+
+  it("should render the default cards", () => {
+    expect(wrapper.find("CardWrapper").prop("items")).toEqual(initialItems);
+    expect(wrapper.find("Card")).toHaveLength(6);
+  });
+
+  it("should open the Dialog with form on click on the Fab", () => {
+    wrapper.find("Fab").simulate("click");
+
+    expect(wrapper.find("Form")).toHaveLength(1);
   });
 });
